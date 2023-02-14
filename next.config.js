ae06399642b70e6436e4e40ed2e916c0f7f46145
hegde45 <bharathegde45@gmail.com>
2023-02-15 02:08:19 +0530

    cache headers removed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: async () => [
-    {
-      source: '/:path*',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'no-cache, no-store, max-age=0, must-revalidate',
-        },
-      ],
-    },
-  ],
 }
 
 module.exports = nextConfig
